31c0bf3da8f66cf8ca8b5565b2b393f88c08b2b7
Your Name <johntweaver@gmail.com>
2020-08-20 19:59:00 -0400

    started

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
